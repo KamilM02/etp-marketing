2ae9de2cb914bb70f683ac8559294098603e889b
Kamil <kamilmrowiec02@gmail.com>
2025-03-16 04:30:46 +0000

    Deploying to GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  basePath: "/event-ticketing-platform/kamil",
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
